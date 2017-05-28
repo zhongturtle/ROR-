ee7134c49d05b949349b333a0353226e5b0b3fba
zhongturtle <zhongturtle@gmail.com>
2017-05-29 00:50:59 +0800

    rails first practices

---
+++ demo/app/controllers/welcome_controller.rb
@@ -0,0 +1,14 @@
+class WelcomeController < ApplicationController
+	# action
+	def index
+		
+	end
+
+
+
+	#GET /welcome/say_hello
+	def say
+		#default go to views/welcome/say.html.erb
+		
+	end
+end
---
+++ demo/config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  resources :people
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/welcome/say_hello" => "welcome#say"
+
+  get "welcome" => "welcome#index"
+
+  #default home page url setting
+  root 'welcome#index'
+end
